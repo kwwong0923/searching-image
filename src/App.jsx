9db56bb1d9c46d5b04e7450f38832edb2c7ed52d
Ka Wai Wong <kw.wong0923@gmail.com>
2023-11-30 17:39:33 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SearchForm from "./SearchForm";
+import Gallery from "./Gallery";
+import ThemeToggle from "./ThemeToggle";
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
---
+++ src/context.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect, createContext, useContext } from "react";
+
+const AppContext = createContext();
+
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+  
+  const storedDarkMode = localStorage.getItem("darkTheme") === "true";
+  
+  return prefersDarkMode || storedDarkMode;
+};
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
+  const [searchTerm, setSearchTerm] = useState("cat");
+
+  useEffect(() => {
+    document.body.classList.toggle("dark-theme", isDarkTheme);
+  }, [isDarkTheme]);
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme)
+  };
+
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
